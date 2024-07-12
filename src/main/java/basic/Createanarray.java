d9d8f7af37c05dbd068ded9d432d72011f8760c5
Dharaneesh <109326864+Dharaneesh500@users.noreply.github.com>
2024-07-12 12:36:58 +0530

    interntask

---
+++ src/main/java/basic/Createanarray.java
@@ -0,0 +1,11 @@
+package basic;
+
+import java.util.Arrays;
+
+public class Createanarray
+{
+    public static void main(String[] args) {
+        String [] names={"Dharaneesh","giri" };
+        System.out.println(Arrays.toString(names));
+    }
+}
